d1d1d55f76d13a9b45454002f1f8556e962101ff
kawadatakuto <takuto.2160@gmail.com>
2020-09-24 20:37:29 +0900

    コメントアウト削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,13 +8,5 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password             { '1a' + Faker::Internet.password(min_length: 7, max_length: 20) }
 
-    # nickname              {Faker::Name.initials(number: 2)}
-    # family_name           {Faker::Name.initials(number: 2)}
-    # first_name            {Faker::Name.initials(number: 2)}
-    # family_name_kana      {Faker::Name.initials(number: 2)}
-    # first_name_kana       {Faker::Name.initials(number: 2)}
-    # email                 {Faker::Internet.free_email}
-    # password              {Faker::Internet.password(min_length: 6)}
-    # password_confirmation {password}
   end
 end
