08236d06a8aec2c0fb65875c022a836cde697b67
kawadatakuto <takuto.2160@gmail.com>
2020-09-28 15:37:32 +0900

    ログインしているユーザーだけが、出品ページへ遷移できる

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,15 @@
+class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
+  
+  def index
+  end
+
+  def new
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to user_session_path
+    end
+  end
+end
