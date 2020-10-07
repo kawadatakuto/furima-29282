7c4c15e0ef7130ee83a8fb01e861e96e8699b07e
kawadatakuto <takuto.2160@gmail.com>
2020-10-07 16:22:13 +0900

    商品詳細表示機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-
+  before_action :set_item, only: [:show, ]
   before_action :move_to_index, except: [:index, :show]
   
   def index
@@ -19,11 +19,19 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private  # private以下の記述はすべてプライベートメソッドになる
 
   #画像の保存を許可する記述
   def item_params
     params.require(:item).permit( :title,:text, :image, :genre_id, :status_id, :delivery_fee_id, :delivery_area_id, :delivery_time_id, :selling_price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @items = Item.find(params[:id])
   end
 
   def move_to_index
